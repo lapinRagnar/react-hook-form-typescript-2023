7682b353072867f140334e93f5a25a57d7bfd4dd
lapinRagnar <lapinragnar@gmail.com>
2023-11-07 19:20:21 +0100

    Custom Validation 1

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -56,6 +56,9 @@ const YoutubeForm = () => {
             pattern: {
               value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
               message: "invalid email address"
+            },
+            validate: (fieldValue) => {
+              return fieldValue !== "admin@example.com" || "enter a different email"
             }
           })} />
 
