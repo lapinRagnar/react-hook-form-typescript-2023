1339a72d0157be7a6d9953eef482c20f7cc0701f
lapinRagnar <lapinragnar@gmail.com>
2023-11-07 18:56:42 +0100

    validation pour text

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -26,22 +26,23 @@ const YoutubeForm = () => {
   return (
     <div>
       <h1>Youtube Form ({renderCount/2})</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor="username">Username</label>
         <input
           type="text"
           id="username"
-          name={name}
-          ref={ref}
-          onChange={onChange}
-          onBlur={onBlur} 
+          {...register("username", {
+            required: "Username is required",
+          })}
         />
 
         <label htmlFor="email">Email</label>
         <input type="email" id="email" {...register("email")} />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel")}/>
+        <input type="text" id="channel" {...register("channel", {
+          required: "Channel is required",
+        })}/>
 
         <button>Submit</button>
       </form>
